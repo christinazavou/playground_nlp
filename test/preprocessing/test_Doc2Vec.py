6146ef0f6beace8dd97551c8f9c05520cee34e36
christinazavou <czavou01@gmail.com>
2020-02-09 21:41:35 +0100

    doc2vec

--- test/preprocessing/test_Doc2Vec.py
+++ test/preprocessing/test_Doc2Vec.py
@@ -5,8 +5,22 @@ class TestDoc2VecUtilsMethods(unittest.TestCase):
 
     def test_something(self):
         from src.preprocessing.Doc2Vec import CorpusToDoc2Vec
-        corpusToDoc2Vec = CorpusToDoc2Vec("exampleData.csv", "textpreprocessed", "exampleModel.p")
-        print("aha")
+        corpusToDoc2Vec = CorpusToDoc2Vec("../exampleData.csv", "textpreprocessed", "exampleModel.p")
+
+        vectors_matrix = corpusToDoc2Vec.get_vectors_as_np()
+        self.assertEqual((10, 400), vectors_matrix.shape)
+
+        sent_60540 = corpusToDoc2Vec.model.docvecs['SENT_60540']
+        self.assertEqual(400, len(sent_60540))
+
+        similar_to_60540 = corpusToDoc2Vec.show_similar('SENT_60540', 3)
+        print(similar_to_60540)
+
+        similarity_of_equal = corpusToDoc2Vec.similarity_given_indices('SENT_60540', 'SENT_60540')
+        print(similarity_of_equal)
 
 if __name__ == '__main__':
     unittest.main()
+
+    # similar_by_vector(vector, topn=10, restrict_vocab=None)
+    # similarity_unseen_docs(model, doc_words1, doc_words2, alpha=0.1, min_alpha=0.0001, steps=5)
--- test/utils/test_pandas_utils.py
+++ test/utils/test_pandas_utils.py
@@ -20,6 +20,33 @@ class TestPandasUtilsMethods(unittest.TestCase):
         self.assertEqual(3, len(result_list[1]))
         self.assertEqual(1, len(result_list[2]))
 
+    def test_get_preprocessed_text(self):
+        from src.utils.pandas_utils import get_preprocessed_text
+        senteces_list = [[u'mon', u'spectrum', u'free'], [u'hostnam', u'ram', u'free', u'swap']]
+
+        expected = "mon spectrum free hostnam ram free swap"
+        self.assertEqual(expected, get_preprocessed_text(senteces_list, use_bi_grams=False, as_list=False))
+
+        expected = "mon spectrum free mon_spectrum spectrum_free hostname ram free swap hostnam_ram ram_free free_swap"
+        expected = list(set(expected.split(" ")))
+        self.assertEqual(len(expected), len(get_preprocessed_text(senteces_list, True, False).split(" ")))
+
+        expected = ["mon", "spectrum", "free", "mon_spectrum", "spectrum_free", "hostnam", "ram", "free", "swap", "hostnam_ram", "ram_free", "free_swap"]
+        expected = list(set(expected))
+        self.assertEqual(len(expected), len(get_preprocessed_text(senteces_list, True, True)))
+
+        expected = ['mon', u'spectrum', u'free', u'hostnam', u'ram', u'free', u'swap']
+        self.assertEqual(expected, get_preprocessed_text(senteces_list, False, True))
+
+    def test_iter_tickets_on_field(self):
+        from src.utils.pandas_utils import iter_tickets_on_field
+        generator = iter_tickets_on_field("textpreprocessed", input_file="../exampleData.csv", use_bi_grams=False, as_list=False)
+        idx, ticket = next(generator)
+        self.assertTrue(isinstance(ticket, str))
+
+        generator = iter_tickets_on_field("textpreprocessed", input_file="../exampleData.csv", use_bi_grams=False, as_list=True)
+        idx, ticket = next(generator)
+        self.assertTrue(isinstance(ticket, list))
 
 if __name__ == '__main__':
     unittest.main()
